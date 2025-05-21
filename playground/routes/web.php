ee35c20d50d91fe57ef45d619eda7f03120fc341
daledp <dpdedoroy@yahoo.com>
2025-05-21 11:05:18 +0800

    trash

--- playground/routes/web.php
+++ playground/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+$firstName = "John";
+$age = 30;
+$hobbies = ["reading","traveling","coding",30,true]; //single dimension array
+$cars = [["honda"],["isuze"],["mazda"]]; //multiple dimension
+// php artisan tinker
+// copy code to terminal
+
+$profile [
+'firstName' => 'John',
+'lastName' => 'Doe'
+]; //associative (dictionary)-- uses key instead of index
+
+$profile = (object) [
+'firstName' => 'John',
+'lastName' => 'Doe'
+]; 
+
+Route::get
+
+
+
