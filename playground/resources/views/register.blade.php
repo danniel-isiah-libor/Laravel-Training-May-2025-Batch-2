47bd091b24c8d8100b726bdf373977f19bbb3d17
danniel-isiah-libor <dlibor.dev@gmail.com>
2025-05-21 11:01:03 +0800

    Day 2: register

---
+++ playground/resources/views/register.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Register </h1>
+
+    <label>Name:</label>
+    <input type="text">
+
+    <br>
+
+    <label>Email:</label>
+    <input type="email">
+
+    <br>
+
+    <label>Password:</label>
+    <input type="password">
+
+    <br>
+
+    <label>Password Confirmation:</label>
+    <input type="password">
+
+    <br>
+
+    <button>Submit</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+    // $variable = "john"; // "<script> alert('HACKED!'); </script>";
+
+    // if (true) {
+    //     echo "<h1> hello world </h1>";
+
+    //     echo htmlspecialchars($variable);
+    // }
+    ?>
+
+    {{-- @if (true)
+        <h1> hello world </h1>
+
+        {!! $variable !!}
+    @endif --}}
+</body>
+</html>
